6e90ac95af5a34fc1618896b110247f84a3f1edf
DanUrvantsev <laorant8@gmail.com>
2025-07-07 23:13:49 +0700

    HW base-concept codebeautify

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,45 +1,44 @@
 "use strict";
 
 function solveEquation(a, b, c) {
- 
-  const discriminant = b ** 2 - 4 * a * c;
-  let arr = [];
 
-  if (discriminant > 0) {
-    const sqrtD = Math.sqrt(discriminant);
-    const root1 = (-b + sqrtD) / (2 * a);
-    const root2 = (-b - sqrtD) / (2 * a);
-    arr.push(root1, root2);
-  } else if (discriminant === 0) {
-    const root = -b / (2 * a);
-    arr.push(root);
-  } else {
-  }
+	const discriminant = b ** 2 - 4 * a * c;
+	let arr = [];
 
-  return arr;
+	if (discriminant > 0) {
+		const sqrtD = Math.sqrt(discriminant);
+		const root1 = (-b + sqrtD) / (2 * a);
+		const root2 = (-b - sqrtD) / (2 * a);
+		arr.push(root1, root2);
+	} else if (discriminant === 0) {
+		const root = -b / (2 * a);
+		arr.push(root);
+	} else {}
+
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 
-  const convert = value => {
-    const num =parseFloat (value);
-    return isNaN(num) ? false : num;
-  };
+	const convert = value => {
+		const num = parseFloat(value);
+		return isNaN(num) ? false : num;
+	};
 
-  const p = convert (percent);
-  let c = convert (contribution);
-  let a = convert (amount);
-  const n = convert (countMonths);
+	const p = convert(percent);
+	let c = convert(contribution);
+	let a = convert(amount);
+	const n = convert(countMonths);
 
-  if([p, c, a, n].includes(false)) return false;
+	if ([p, c, a, n].includes(false)) return false;
 
-  c = Math.min (c, a);
-  a = Math.max (a-c, 0);
+	c = Math.min(c, a);
+	a = Math.max(a - c, 0);
 
-  const montlyRate = p / 100 / 12;
+	const montlyRate = p / 100 / 12;
 
-  const paymant = a * ( montlyRate + montlyRate / ((1+montlyRate) ** n -1));
-  
-  const total = paymant * n;
-  return Math.round(total * 100) / 100;
+	const paymant = a * (montlyRate + montlyRate / ((1 + montlyRate) ** n - 1));
+
+	const total = paymant * n;
+	return Math.round(total * 100) / 100;
 }
