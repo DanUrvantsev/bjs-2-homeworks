4072b4f5293873e6283a13fe42a8f40f3bb2018d
DanUrvantsev <laorant8@gmail.com>
2025-07-17 21:36:57 +0700

    HW decorators

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,56 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+  const cache = [];
+  return function (...args) {
+    const hash = md5(args);
+    const cachedItem = cache.find((item) => item.hash === hash);
+    if (cachedItem) {
+      return "Из кеша: " + cachedItem.value;
+    }
+
+    const result = func(...args);
+
+    cache.push({
+      hash: hash,
+      value: result,
+    });
+
+    if (cache.length > 5) {
+      cache.shift();
+    }
+
+    return "Вычисляем: " + result;
+  };
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
+  let timeoutId = null;
+  let firstCall = true;
   
+  function wrapper(...args) {
+    wrapper.allCount++;
+    
+    if (firstCall) {
+      func.apply(this, args);
+      wrapper.count++;
+      firstCall = false;
+
+      timeoutId = setTimeout(() => {
+        timeoutId = null;
+      }, delay);
+    } else {
+      clearTimeout(timeoutId);
+
+      timeoutId = setTimeout(() => {
+        func.apply(this, args);
+        wrapper.count++;
+      }, delay);
+    }
+  }
+
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+  
+  return wrapper;
 }
